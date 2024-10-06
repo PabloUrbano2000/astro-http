e2f1a1eb13cb5f97c0b88363f771339fb2c1a2dd
Pablo El Maldy <pablojamiro2008@hotmail.com>
2024-10-06 14:42:37 -0500

    cloudflare

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,14 +4,14 @@ import { defineConfig } from 'astro/config'
 
 import sitemap from '@astrojs/sitemap'
 
-import node from '@astrojs/node'
+// import node from '@astrojs/node'
+
+import cloudflare from '@astrojs/cloudflare'
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://example.com',
   integrations: [mdx(), sitemap()],
   output: 'hybrid',
-  adapter: node({
-    mode: 'standalone'
-  })
+  adapter: cloudflare()
 })
